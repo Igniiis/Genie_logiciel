34be6eed56a8354f7dbe4c648203886f01e6b5b9
MaxenceM <66200976+Igniiis@users.noreply.github.com>
2024-05-26 18:05:34 +0200

    Add files via upload

---
+++ bdd/script.sql
@@ -0,0 +1,41 @@
+--Réinitialisation
+-- Drop the tables in the correct order to handle dependencies
+DROP TABLE IF EXISTS client_vehicule CASCADE;
+DROP TABLE IF EXISTS client CASCADE;
+DROP TABLE IF EXISTS vehicule CASCADE;
+DROP TABLE IF EXISTS Borne CASCADE;
+
+-- Drop the domain
+DROP DOMAIN IF EXISTS enum_etat_borne;
+
+
+--Création des tables
+CREATE TABLE client (
+    id_client SERIAL PRIMARY KEY,
+    nom_client VARCHAR(50) NOT NULL,
+    prenom_client VARCHAR(50) NOT NULL,
+    adresse_client VARCHAR(255) NOT NULL UNIQUE,
+    password_client VARCHAR(255) NOT NULL,
+    num_tel_client VARCHAR(15)
+);
+
+CREATE TABLE vehicule (
+	plaque_vehicule VARCHAR(9) NOT NULL PRIMARY KEY
+);
+
+CREATE TABLE client_vehicule (
+    id_client INTEGER NOT NULL,
+    plaque_vehicule VARCHAR(9) NOT NULL,
+    PRIMARY KEY (id_client, plaque_vehicule),
+    FOREIGN KEY (id_client) REFERENCES client(id_client) ON DELETE CASCADE,
+    FOREIGN KEY (plaque_vehicule) REFERENCES vehicule(plaque_vehicule) ON DELETE CASCADE
+);
+
+CREATE DOMAIN enum_etat_borne VARCHAR(50) NOT NULL CHECK
+    (VALUE IN ('disponible', 'reservé', 'indisponible'));
+
+
+CREATE TABLE Borne (
+    id_borne SERIAL PRIMARY KEY,
+    etat_borne enum_etat_borne NOT NULL DEFAULT 'indisponible'
+);
